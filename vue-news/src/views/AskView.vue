a1b6b302424be83e54c05af3f1e49b1bfb042b5f
mijinB <bmj13465@gmail.com>
2024-02-26 22:12:33 +0900

    vue router 설정

---
+++ vue-news/src/views/AskView.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>ask</div>
+</template>
+<script>
+export default {};
+</script>
---
+++ vue-news/src/views/NewsView.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>news</div>
+</template>
+<script>
+export default {};
+</script>
