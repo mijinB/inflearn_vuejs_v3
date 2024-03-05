ace7515f4771d223c577ac282a640bfccd59ddd2
Bmijin <bmj13465@jtts.co.kr>
2024-03-05 10:03:19 +0900

    css 스타일링

--- vue-news/src/views/AsksView.vue
+++ vue-news/src/views/AsksView.vue
@@ -1,11 +1,23 @@
 <template>
     <div>
-        <p v-for="ask in fetchedAsks" v-bind:key="ask.id">
-            <router-link :to="`item/${ask.id}`">
-                {{ ask.title }}
-            </router-link>
-            <small>{{ ask.time_ago }} by {{ ask.user }}</small>
-        </p>
+        <ul class="news-list">
+            <li v-for="item in fetchedAsks" v-bind:key="item.id" class="post">
+                <!-- 포인트 영역 -->
+                <div class="points">
+                    {{ item.points }}
+                </div>
+                <!-- 기타 정보 영역 -->
+                <p class="news-title">
+                    <router-link :to="`item/${item.id}`">
+                        {{ item.title }}
+                    </router-link>
+                </p>
+                <small class="link-text">
+                    {{ item.time_ago }} by 
+                    <router-link :to="`user/${item.user}`" class="link-text">{{ item.user }}</router-link>
+                </small>
+            </li>
+        </ul>
     </div>
 </template>
 <script>
@@ -33,3 +45,30 @@ export default {
     }
 };
 </script>
+
+<style scoped>
+.news-list {
+    margin: 0;
+    padding: 0;
+}
+.post {
+    display: flex;
+    align-items: center;
+    border-bottom: 1px solid #eee;
+    list-style: none;
+}
+.points {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 80px;
+    height: 60px;
+    color: #42b883;
+}
+.news-title {
+    margin: 0;
+}
+.link-text {
+    color: #828282;
+}
+</style>
--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -1,16 +1,26 @@
 <template>
     <div>
-        <p v-for="user in this.$store.state.news" v-bind:key="user.id">
-            <a v-bind:href="user.url">
-                {{ user.title }}
-            </a>
-            <small>
-                {{ user.time_ago }} by 
-                <router-link :to="`user/${user.user}`">{{ user.user }}</router-link>
-            </small>
-        </p>
+        <ul class="news-list">
+            <li v-for="item in this.$store.state.news" v-bind:key="item.id" class="post">
+                <!-- 포인트 영역 -->
+                <div class="points">
+                    {{ item.points }}
+                </div>
+                <!-- 기타 정보 영역 -->
+                <p class="news-title">
+                    <a v-bind:href="item.url">
+                        {{ item.title }}
+                    </a>
+                </p>
+                <small class="link-text">
+                    {{ item.time_ago }} by 
+                    <router-link :to="`user/${item.user}`" class="link-text">{{ item.user }}</router-link>
+                </small>
+            </li>
+        </ul>
     </div>
 </template>
+
 <script>
 export default {
     created() {
@@ -18,3 +28,30 @@ export default {
     },
 };
 </script>
+
+<style scoped>
+.news-list {
+    margin: 0;
+    padding: 0;
+}
+.post {
+    display: flex;
+    align-items: center;
+    border-bottom: 1px solid #eee;
+    list-style: none;
+}
+.points {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 80px;
+    height: 60px;
+    color: #42b883;
+}
+.news-title {
+    margin: 0;
+}
+.link-text {
+    color: #828282;
+}
+</style>
