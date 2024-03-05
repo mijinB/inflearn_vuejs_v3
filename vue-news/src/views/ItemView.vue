fa80a4dd2756257a13073579ffe9f75ec58dadeb
Bmijin <bmj13465@jtts.co.kr>
2024-03-05 15:46:20 +0900

    컴포넌트 적용 및 slot 사용

--- vue-news/src/views/ItemView.vue
+++ vue-news/src/views/ItemView.vue
@@ -1,20 +1,14 @@
 <template>
   <div>
     <section>
-      <!-- 질문 상세 정보 -->
-      <div class="user-container">
-        <div>
-          <i class="fa-solid fa-user"></i>
-        </div>
-        <div class="user-description">
-          <router-link :to="`/user/${fetchedItem.user}`">
-            {{ fetchedItem.user }}
-          </router-link>
-          <div class="time">
-            {{ fetchedItem.time_ago }}
-          </div>
-        </div>
-      </div>
+      <!-- 사용자 정보 -->
+      <UserProfile :info="fetchedItem">
+        <div slot="username">{{ fetchedItem.user }}</div>
+        <template slot="time">{{ fetchedItem.time_ago }}</template>
+      </UserProfile>
+    </section>
+
+    <section>
       <h2>{{ fetchedItem.title }}</h2>
     </section>
 
@@ -27,8 +21,12 @@
 
 <script>
 import { mapGetters } from 'vuex';
+import UserProfile from '@/components/UserProfile.vue';
 
 export default {
+  components: {
+    UserProfile,
+  },
   computed: {
     ...mapGetters(['fetchedItem']),
   },
