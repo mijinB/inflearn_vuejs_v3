94564db2b5a56ccce2a414ea9bcf231d3b52fc6c
Bmijin <bmj13465@jtts.co.kr>
2024-03-05 16:41:27 +0900

    bus사용해서 spinner 구현

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -4,16 +4,41 @@
     <transition name="page">
       <router-view></router-view>
     </transition>
+    <SpinnerCompo :loading="loadingStatus" />
   </div>
 </template>
 
 <script>
 import ToolBar from './components/ToolBar.vue';
+import SpinnerCompo from './components/SpinnerCompo.vue';
+import bus from './utils/bus';
 
 export default {
   components: {
     ToolBar,
+    SpinnerCompo,
   },
+  data() {
+    return {
+      loadingStatus: false,
+    }
+  },
+  methods: {
+    startSpinner() {
+      this.loadingStatus = true;
+    },
+    endSpinner() {
+      this.loadingStatus = false;
+    },
+  },
+  created() {
+    bus.$on('start:spinner', this.startSpinner);
+    bus.$on('end:spinner', this.endSpinner);
+  },
+  beforeDestroy() {
+    bus.$off('start:spinner', this.startSpinner);
+    bus.$off('end:spinner', this.endSpinner);
+  }
 }
 </script>
 
