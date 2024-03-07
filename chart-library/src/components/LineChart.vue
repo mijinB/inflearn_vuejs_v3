a97b3ae63c36d4ea010576185e20a0c4719e9336
Bmijin <bmj13465@jtts.co.kr>
2024-03-07 10:31:57 +0900

    line chart 추가 구현

---
+++ chart-library/src/components/LineChart.vue
@@ -0,0 +1,26 @@
+<template>
+  <canvas id="lineChart"></canvas>
+</template>
+
+<script>
+import { Chart, registerables } from 'chart.js'
+Chart.register(...registerables)
+
+export default {
+    mounted() {
+        var ctx = document.getElementById('lineChart').getContext('2d');
+        new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+                datasets: [{
+                    label: 'My First dataset',
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: [0, 10, 5, 2, 20, 30, 45],
+                }]
+            }
+        })
+    }
+}
+</script>
