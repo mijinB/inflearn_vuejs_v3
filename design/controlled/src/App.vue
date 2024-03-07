9a3256e433fc4fe1c1012e6e255c23c21bdecc3e
Bmijin <bmj13465@jtts.co.kr>
2024-03-07 14:41:07 +0900

    controlled 컴포넌트 적용

--- design/controlled/src/App.vue
+++ design/controlled/src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <check-box></check-box>
+  <!-- props 대신 v-model로 data 보냄 -->
+  <check-box v-model="checked"></check-box>
 </template>
 
 <script>
@@ -9,5 +10,10 @@ export default {
   components: {
     CheckBox
   },
+  data() {
+    return {
+      checked: false
+    }
+  }
 }
 </script>
--- design/controlled/src/components/CheckBox.vue
+++ design/controlled/src/components/CheckBox.vue
@@ -1,9 +1,14 @@
 <template>
-  <input type="checkbox">
+  <input type="checkbox" :value="value" @click="toggleCheckBox">
 </template>
 
 <script>
 export default {
-  
+  props: ['value'], // App.vue에서 v-model로 내려준 checked가 value로 옴
+  methods: {
+    toggleCheckBox() {
+      this.$emit('input', !this.value);
+    }
+  }
 }
 </script>
